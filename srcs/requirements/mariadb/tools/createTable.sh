7532933848a37e1daa0e1dc3d18fdac5f8220340
atomasi <atomasi@Inception.myguest.virtualbox.org>
2026-03-30 22:06:16 +0200

    mariadb maybe works fine, need to learn how the code works like that

---
+++ srcs/requirements/mariadb/tools/createTable.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#setup dir for socket
+
+mkdir -p /run/mysqld
+chown mysql:mysql /run/mysqld
+
+
+#start service
+service mariadb start;
+#create table
+mysql -e "CREATE DATABASE IF NOT EXISTS \`$MARIADB_DATABASE\`;"
+
+#waiting mariadb start
+until mysqladmin ping > /dev/null 2>&1; do
+	echo "waiting starting mariadb..."
+	sleep 1
+done
+
+#create user
+mysql -e "CREATE USER IF NOT EXISTS \`$MARIADB_USER\`@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+#allowed rights for the table to the user
+mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+#change root user's password
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
+#update change
+mysql -e "FLUSH PRIVILEGES;"
+#shut down msyql
+mysqladmin -u root -p$MARIADB_ROOT_PASSWORD shutdown
+#restart mysql
+exec mysqld_safe --user=mysql
